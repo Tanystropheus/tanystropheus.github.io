e8d7c94753eddf10d06b3f13ceb35c63c3345578
Patate Craouzette <poubelllle4242@gmail.com>
2016-05-17 23:32:47 +0200

    changement intégral du DOM pour régler les bugs du click, drag and drop chekc, click check, bugs de CSS régler, manque l'ajout dynamique des elements, les scrolls, le bouton suppression fonctionnel, l'organisation des onglets

--- myVoice/www/js/myscript.js
+++ myVoice/www/js/myscript.js
@@ -1,47 +1,47 @@
  // Switch mode
-            $(document).ready(function(){
-                $("#button_param").click(function() {
-                    if ($('#select-based-flipswitch').val() == "leave") {
-                        $.mobile.changePage('#usepage', { transition: "slide"} );
-                    }
-                    else if ($('#select-based-flipswitch').val() == "arrive") {
-                        // Check form
-                        $("#popupSave").popup("open");
-                        $("#button_form").click(function() {
-                            if ($("#fname").val().length > 0 
-                                && $("#factivity").val().length > 0
-                                && $("#flocation").val().length > 0) {
-                                    $.mobile.changePage('#learningpage', { transition: "slide"} );
-                            }
-                            else {
-                                console.log("ERREUR");
-                            }
-                        });
-                    }
-                });
-
-                // sortable and drag & drop elements
-                $( ".column" ).sortable({
-                    connectWith: ".column",
-                    handle: ".portlet-content",
-                    placeholder: "portlet-placeholder ui-corner-all"
-                });
-             
-                $( ".portlet" )
-                    .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-                    .find( ".portlet-header" )
-                    .addClass( "ui-widget-header ui-corner-all" )
-                    .find( ".portlet-footer" )
-                    .addClass( "ui-widget-header ui-corner-all" );
-            });
-
-            // Click
-            $(document).on("tap", ".portlet", function(){
-                if ($(this).parent().parent().attr("class") == "elementArea") {
-                    $(this).prependTo(".column");
-                }
-                else if ($(this).parent().parent().attr("class") == "speakArea") {
-                    var desc = $(".elementArea").children().children();
-                    $(this).prependTo(desc);
+$(document).ready(function() {
+    $("#button_param").click(function() {
+//        console.log("event via 1 : " + $(this).attr("id"));
+        if ($('#select-based-flipswitch').val() == "leave") {
+            $.mobile.changePage('#usepage', { transition: "slide"} );
+        }
+        else if ($('#select-based-flipswitch').val() == "arrive") {
+            // Check form
+            $("#popupSave").popup("open");
+            $("#button_form").click(function() {
+                if ($("#fname").val().length > 0 
+                    && $("#factivity").val().length > 0
+                    && $("#flocation").val().length > 0) {
+                        $.mobile.changePage('#learningpage', { transition: "slide"} );
                 }
             });
+        }
+    });
+
+    // sortable elements
+    $("#sortable-el, #sortable-sp, #sortable-elT, #sortable-spT").sortable({
+        connectWith: ".connectedSortable",
+        dropOnEmpty: true,
+        placeholder: "ui-state-highlight",
+    }).disableSelection();
+});
+
+// Click
+$(document).on("tap", ".element", function() {
+    if ($(this).parent().attr("id") == "sortable-el") {
+        console.log("plop 1");
+        $(this).prependTo("#sortable-sp");
+    }
+    else if ($(this).parent().attr("id") == "sortable-sp") {
+        console.log("plop 2");
+        $(this).prependTo("#sortable-el");
+    }
+    else if ($(this).parent().attr("id") == "sortable-elT") {
+        console.log("plop 3");
+        $(this).prependTo("#sortable-spT");
+    }
+    else if ($(this).parent().attr("id") == "sortable-spT") {
+        console.log("plop 4");
+        $(this).prependTo("#sortable-elT");
+    }
+});
