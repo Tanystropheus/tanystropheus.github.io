66b0c5379b33040143c94161e385f15cb2ec8dde
Patate Craouzette <poubelllle4242@gmail.com>
2016-05-23 16:29:42 +0200

    ajout des images dynamique, merci Mathieu ;)

--- myVoice/www/js/myscript.js
+++ myVoice/www/js/myscript.js
@@ -28,6 +28,24 @@ $(document).ready(function() {
         opacity: 0.5,
         tolerance: "intersect"
     }).disableSelection();
+
+    // add elements
+    var tmp = {"img/carre1.png" : 8, 
+    		"img/carre2.jpg" : 10, 
+    		"img/carre3.png" : 2, 
+    		"img/carre4.png" : 1,  
+    		"img/carre5.jpg" : 4, 
+    		"img/carre6.jpg" : 5, 
+    		"img/carre7.jpg" : 6, 
+    		"img/carre8.jpg" : 7};
+	var str = "";
+
+	for(var i in tmp) {
+    	str = str + '<li class="element">'+
+    		tmp[i] + '<ul><li><img class="imgClass" src='+
+    		i + ' alt="Test' + tmp[i] + '"/></li></ul></li>';
+	}
+    $('#sortable-el').html(str);
 });
 
 function movePict(e) {
